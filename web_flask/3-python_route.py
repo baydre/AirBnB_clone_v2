0cef27f4f3db2fd4caaf4083b05de01a2a6b4024
baydre_africa <baydreafrica@gmail.com>
2024-01-02 13:05:47 +0100

    script that display 'Python ', followed by the value of the text variable

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
+Task 3: Python is cool!
 """
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
     """
+    display "Hello HBNB"
     """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """
+    display "HBNB"
     """
     return "HBNB"
 
@@ -24,16 +25,20 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
+    display "C ", followed by the value
+    of the text variable.
     """
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """
+    display "Python ", followed by the value
+    of the text variable.
     """
-    return ("Python " + text.replace("_", " "))
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
